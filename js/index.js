0b3ec995ee3062ca6738b8829b40d204bb904ee0
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2024-08-29 15:36:00 +0300

    add buger menu, create basic form, style for input

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+const toggleBurger = document.querySelector('.burger');
+const nav = document.querySelector('.header__nav');
+
+toggleBurger.addEventListener('click', function(){
+   toggleBurger.classList.toggle('active');
+   nav.classList.toggle('active');
+});
+
 function getMyFunction(){
    alert("Hi!")
 }
